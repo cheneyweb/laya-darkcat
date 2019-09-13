4643649f0678490282601ad475bd9196a89eae89
cheneyweb <457299596@qq.com>
2019-09-14 01:00:54 +0800

    对接微信

--- server/src/controller/player.js
+++ server/src/controller/player.js
@@ -91,8 +91,8 @@ router.get('/earn', async (ctx, next) => {
             ShareMap[playerToday] = 0
         }
         if (ShareMap[playerToday] || ShareMap[playerToday] == 0) {
-            if (ShareMap[`${token._id}${playerToday}`] < 3) {
-                ShareMap[`${token._id}${playerToday}`]++
+            if (ShareMap[playerToday] < 3) {
+                ShareMap[playerToday]++
                 goldInc = 50
             }
         }
