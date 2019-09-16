7961f198c78f12845eccc607be7fb6d034e089bd
cheneyweb <457299596@qq.com>
2019-09-17 00:31:09 +0800

    发布预上线版本

--- src/script/Guide.js
+++ src/script/Guide.js
@@ -9,7 +9,8 @@ export default class Guide extends Laya.Script {
     onTriggerEnter(other, self, contact) {
         if (other.label == "soldier") {
             if (other.owner.getComponent(Laya.Script).isFreedom()) {
-                Math.random() < 0.2 && Laya.SoundManager.playSound("sound/tease.mp3")
+                // Math.random() < 0.5 && 
+                Laya.SoundManager.playSound("sound/tease.mp3")
                 this.owner.visible = false
                 this.owner.pos(0, 0)
             }
